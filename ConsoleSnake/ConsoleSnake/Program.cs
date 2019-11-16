6c8194495befee21a8ddc5cbe590254702afd4d6
Dima <dimi4ik44@gmail.com>
2019-11-17 01:25:17 +0300

    Test Hello World

--- ConsoleSnake/ConsoleSnake/Program.cs
+++ ConsoleSnake/ConsoleSnake/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleSnake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hell");
+            Console.WriteLine("Hello World");
         }
     }
 }
